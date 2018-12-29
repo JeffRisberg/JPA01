30a955c5285ab7ba5e4b62c7f47356f8e6e9dfa7
JeffRisberg <jrisberg@aol.com>
2018-12-28 21:37:55 -0800

    added service and transaction management

---
+++ src/main/java/com/company/services/AbstractService.java
@@ -0,0 +1,38 @@
+package com.company.services;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import java.util.function.Consumer;
+
+/**
+ * @author Jeff Risberg
+ * @since 11/11/17
+ */
+@Slf4j
+public class AbstractService {
+
+    static public EntityManagerFactory emf;
+    // = Persistence.createEntityManagerFactory("jersey02");
+
+    public void doWork(Consumer<EntityManager> consumer) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            EntityTransaction transaction = em.getTransaction();
+            transaction.begin();
+            try {
+                // call consumer
+                consumer.accept(em);
+                transaction.commit();
+            } catch (Exception e) {
+                log.error("Failed to execute transaction", e);
+                transaction.rollback();
+            }
+        } finally {
+            em.close();
+        }
+    }
+}
--- src/main/java/com/company/services/CharityService.java
+++ src/main/java/com/company/services/CharityService.java
@@ -1,4 +1,35 @@
 package com.company.services;
 
-public class CharityService {
+import com.company.domain.Charity;
+import com.company.services.DAO.CharityDAO;
+
+import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CharityService extends AbstractService {
+    private static CharityDAO dao = new CharityDAO();
+
+    @Inject
+    public CharityService(MySQLConnectionFactory mysqlConnectionFactory) {
+        Map<String, Object> props = new HashMap<String, Object>();
+        props.put("javax.persistence.nonJtaDataSource", mysqlConnectionFactory.getOrCreateDataSource());
+        props.put("javax.persistence.transactionType", "RESOURCE_LOCAL");
+        this.emf = Persistence.createEntityManagerFactory("persistenceUnitName", props);
+    }
+
+    public Charity getById(Long id) {
+        final AtomicReference<Charity> td = new AtomicReference<>();
+        doWork(em -> td.set(dao.getById(id, em)));
+        return td.get();
+    }
+
+    public List<Charity> getCharities(int limit, int offset) {
+        final AtomicReference<List<Charity>> td = new AtomicReference<>();
+        doWork(em -> td.set(dao.listAll(Charity.class, em)));
+        return td.get();
+    }
 }
+
